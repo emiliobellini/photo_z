a37de215377840dbf058c1088164b6bb62585334
Emilio Bellini <emilio.bellini@gmail.com>
2018-01-25 18:45:45 +0000

    partial update

--- binned_photo-z.py
+++ binned_photo-z.py
@@ -37,8 +37,9 @@ good_fit_patterns = ['W1m0m0', 'W1m0m3', 'W1m0m4', 'W1m0p1', 'W1m0p2', 'W1m0p3',
 
 # Parse the given arguments
 parser = argparse.ArgumentParser("Generate plots of binned Photo-z")
-parser.add_argument("input_path", type=str, help="Input TSV file")
+parser.add_argument("input_path", type=str, help="Input FITS path")
 parser.add_argument("--output_file", "-o", type=str, default = None, help="Output file")
+parser.add_argument("--output_plot", "-p", type=str, default = None, help="Output plot")
 args = parser.parse_args()
 
 
@@ -51,28 +52,50 @@ for fn in os.listdir(split_input[0]):
         if fnmatch.fnmatch(fn, split_input[1]+'*'):
             paths['input'].append(split_input[0] + '/' + fn)
 if args.output_file:
-    paths['output'] = os.path.abspath(args.output_file)
+    paths['output_file'] = os.path.abspath(args.output_file)
 else:
-    paths['output'] = os.path.abspath(args.input_path)
-    paths['output'] = paths['output'].rstrip('_')
-    paths['output'] = paths['output'] + '.pdf'
+    paths['output_file'] = os.path.abspath(args.input_path)
+    paths['output_file'] = os.path.splitext(paths['output_file'])[0]
+    paths['output_file'] = paths['output_file'].rstrip('_')
+    paths['output_file'] = paths['output_file'] + '_bins.fits'
+if args.output_plot:
+    paths['output_plot'] = os.path.abspath(args.output_plot)
+else:
+    paths['output_plot'] = os.path.splitext(paths['output_file'])[0]
+    paths['output_plot'] = paths['output_plot'] + '.pdf'
 
 
 #Open files and store datas
-data = []
+table = []
 image = []
 for fn in paths['input']:
-    data.append(fits.open(fn)[1].data)
+    table.append(fits.open(fn)[1].data)
     image.append(fits.open(fn)[2].data)
 
 
-def good_fit_pos(data, z_min, z_max):
+#Criteria used to select the data
+def select_data_pos(data, z_min, z_max):
     pos = np.argwhere(data['Z_B']>=z_min).flatten()
     pos = np.intersect1d(pos, np.argwhere(data['Z_B']<z_max).flatten())
     pos = np.intersect1d(pos, np.argwhere(data['MASK']<=1).flatten())
-    pos = np.intersect1d(pos, np.argwhere(data['field'] in good_fit_patterns).flatten())
+#    pos = np.intersect1d(pos, np.argwhere(data['field'] in good_fit_patterns).flatten())
 
-    return pos
+    return np.array(pos)
 
-for count in range(len(data)):
-    print good_fit_pos(data[count], 1., 1.01)
+
+#Get an array with positions of data for each bin
+pos_bins = {}
+for [z_min, z_max] in z_bins:
+    key = str(z_min)+'<z_B<'+str(z_max)
+    pos_bins[key] = []
+    for count in range(len(table)):
+        pos_bins[key].append(select_data_pos(table[count], z_min, z_max))
+
+
+#Generate binned Photo-z
+z_step = 0.05
+photoz_x = (np.arange(len(image[0][0]))+1./2.)*z_step
+photoz_y = {}
+for key in pos_bins.keys():
+    for count in range(len(image)):
+        print pos_bins[key][count]
