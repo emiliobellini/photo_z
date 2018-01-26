f50d812e013f294c3a2ae261674ab489df7c09c2
Emilio Bellini <emilio.bellini@gmail.com>
2018-01-26 12:45:25 +0000

    script to unify different fits files

---
+++ unify_fits.py
@@ -0,0 +1,79 @@
+import os, sys, fnmatch
+import argparse
+import numpy as np
+from astropy.io import fits
+from astropy.table import Table, vstack
+
+
+# Parse the given arguments
+parser = argparse.ArgumentParser("Unify several fits files into a single one")
+parser.add_argument("input_path", type=str, help="Input FITS path")
+parser.add_argument("--output_file", "-o", type=str, default = None, help="Output file")
+args = parser.parse_args()
+
+
+#Define absolute paths to input and output files
+paths = {}
+split_input = os.path.split(os.path.abspath(args.input_path))
+paths['input'] = []
+for fn in os.listdir(split_input[0]):
+    if fnmatch.fnmatch(fn, '*.fits'):
+        if fnmatch.fnmatch(fn, split_input[1]+'*'):
+            paths['input'].append(split_input[0] + '/' + fn)
+if args.output_file:
+    paths['output'] = os.path.abspath(args.output_file)
+else:
+    paths['output'] = os.path.abspath(args.input_path)
+    paths['output'] = paths['output'].rstrip('_')
+    paths['output'] = paths['output'] + '.fits'
+
+
+#Open all the files and join them in a single one
+
+#Create the first empty image
+hdu = fits.PrimaryHDU()
+hdul = fits.HDUList([hdu])
+
+#Create table
+del hdu
+for fn in paths['input']:
+    try:
+        hdu = vstack([hdu, Table.read(fn)], join_type='exact')
+    except NameError:
+        hdu = Table.read(fn)
+hdu = fits.table_to_hdu(hdu)
+hdul.append(hdu)
+print 'Created final table'
+sys.stdout.flush()
+#Create images
+for key in ['PZ_full']:
+    del hdu
+    for fn in paths['input']:
+        try:
+            hdu = np.vstack((hdu, fits.open(fn)[key].data))
+        except NameError:
+            hdu = fits.open(fn)[key].data
+    hdu = fits.ImageHDU(hdu, name=key)
+    hdul.append(hdu)
+    print 'Created final image for ' + key
+    sys.stdout.flush()
+
+#Write the output file
+try:
+    os.remove(paths['output'])
+except:
+    pass
+hdul.writeto(paths['output'])
+print 'Written output file at ' + os.path.relpath(paths['output'])
+sys.stdout.flush()
+
+# #Remove partial files
+# for fn in paths['input']:
+#     try:
+#         os.remove(fn)
+#     except:
+#         pass
+
+print 'Success!!'
+
+sys.exit()
