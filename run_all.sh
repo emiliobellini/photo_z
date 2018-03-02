14b4f22924affb0482f2b7eb7fbed2da94165b02
Emilio Bellini <emilio.bellini@gmail.com>
2018-03-02 09:04:20 +0000

    Added 5_kl_transform to run_all

--- run_all.sh
+++ run_all.sh
@@ -9,7 +9,10 @@ echo "Finished 2_unify_fits"
 /usr/local/shared/python/2.7.6-gcc/bin/python 3_binned_data.py ../data/data_cfhtlens.fits
 echo "Finished 3_binned_photo-z"
 
-/usr/local/shared/python/2.7.6-gcc/bin/python 4_bins_to_cl.py -s ../data/data_cfhtlens_bins.fits
+/usr/local/shared/python/2.7.6-gcc/bin/python 4_bins_to_cl.py ../data/data_cfhtlens_bins.fits
 echo "Finished 4_bins_to_cl"
+
+/usr/local/shared/python/2.7.6-gcc/bin/python 5_kl_transform.py ../data/data_cfhtlens_bins.fits
+echo "Finished 5_kl_transform"
 
 #addqueue -c '5hours' -q cmb -m 40 -n 1 ./run_all.sh
